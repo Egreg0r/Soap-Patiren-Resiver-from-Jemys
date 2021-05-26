707ead222919e3b4e2056c264cd9596bd2e2d1d9
Egreg0r <maximmus2510@gmail.com>
2021-05-26 14:35:40 +1000

    11

---
+++ SoapPatientResiver/PattientAttachment..cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace attach.utils
+{
+    class PAttachment
+    {
+        public void XmlZaprosCreate ()
+        {
+            string fileName = "250200_11111.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            WebRequest request = WebRequest.Create("http://192.168.1.210:8080/mis/rest/queries?method=getQueryXmlResult&query=Select%20pa.personalData%20FROM%20PatientAttachment%20pa%20WHERE%20pa.id=181");
+            WebResponse response = request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                xmlDoc.Load(stream);
+            }
+            xmlDoc.Save(fileName);
+        }
+
+        void GetElemValue (XDocument docName, string valElement) // вовод первого значение под тего valElement из docName
+        {
+            var v= docName.Descendants(valElement).FirstOrDefault().Value;
+        }
+    }
+}
---
+++ SoapPatientResiver/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("Выгрузка версия 1.0.94")]
+[assembly: AssemblyDescription("Insert description here")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Insert company here")]
+[assembly: AssemblyProduct("MYL and SMM")]
+[assembly: AssemblyCopyright("Insert copyright here")]
+[assembly: AssemblyTrademark("Insert trademark here")]
+[assembly: AssemblyCulture("")]
+
+// Version informationr(
+[assembly: AssemblyVersion("1.0.94.1")]
+[assembly: AssemblyFileVersion("1.0.94.1")]
+[assembly: NeutralResourcesLanguage("ru")]
+
